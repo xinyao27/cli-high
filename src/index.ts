4038ef99f0f588976d125689a87403bcc20145d0
Xinyao <hi@xinyao.me>
2024-05-17 09:44:05 +0800

    feat: higher performance

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,78 @@
-import { highlight as highlightAsHtml } from 'sugar-high'
+import { tokenize } from 'sugar-high'
 import { black, cyanBright, dim, greenBright, magentaBright, redBright } from 'yoctocolors'
 
 export interface HighlightOptions {
   showLineNumbers?: boolean
 }
 export function highlight(code: string, options: HighlightOptions = { showLineNumbers: false }) {
-  const html = highlightAsHtml(code)
-  const regex = /<span class="sh__line">(.*)<\/span>/g
-
-  let match
-  let result = ''
+  const tokens = tokenize(code)
+  const lines: string[] = []
   let i = 1
+  const lineTokens: Array<[number, string]> = []
 
-  // eslint-disable-next-line no-cond-assign
-  while ((match = regex.exec(html)) !== null) {
-    const innerMatches = [...match[1].matchAll(/<span class="([^"]+)" style="color: ([^"]+)">(.*?)<\/span>/gs)]
-    result += options.showLineNumbers ? `${dim(`${i}`.padEnd(2, ' '))} ` : ''
-    innerMatches.forEach((innerMatch) => {
-      const classType = innerMatch[1]
-      const text = decode(innerMatch[3])
-
-      switch (classType) {
-        case 'sh__token--identifier':
-          result += black(text)
-          break
-        case 'sh__token--keyword':
-          result += redBright(text)
-          break
-        case 'sh__token--string':
-          result += greenBright(text)
-          break
-        case 'sh__token--class':
-          result += cyanBright(text)
-          break
-        case 'sh__token--property':
-          result += cyanBright(text)
-          break
-        case 'sh__token--entity':
-          result += magentaBright(text)
-          break
-        case 'sh__token--jsxliterals':
-          result += greenBright(text)
-          break
-        case 'sh__token--sign':
-          result += dim(text)
-          break
-        case 'sh__token--comment':
-          result += dim(text)
-          break
-        default:
-          result += text
-          break
-      }
-    })
-    result += '\n'
+  function createLine(content: string) {
+    const prefix = options.showLineNumbers ? `${dim(`${i}`.padEnd(2, ' '))} ` : ''
     i++
+    return `${prefix}${content}`
+  }
+  function flushLine(tokens: Array<[number, string]>) {
+    lines.push(
+      createLine(
+        tokens
+          .map(([type, value]) => {
+            switch (type) {
+              case 0: // identifier
+                return black(value)
+              case 1: // keyword
+                return redBright(value)
+              case 2: // string
+                return greenBright(value)
+              case 3: // Class, number and null
+                return cyanBright(value)
+              case 4: // property
+                return cyanBright(value)
+              case 5: // entity
+                return magentaBright(value)
+              case 6: // jsx literals
+                return greenBright(value)
+              case 7: // sign
+                return dim(value)
+              case 8: // comment
+                return dim(value)
+              default:
+                return value
+            }
+          })
+          .join(''),
+      ),
+    )
   }
 
-  return result
-}
+  for (const token of tokens) {
+    const [type, value] = token
+    // Skip "break" tokens
+    if (type !== 9) {
+      // Divide multi-line token into multi-line code
+      if (value.includes('\n')) {
+        const lines = value.split('\n')
+        for (let j = 0; j < lines.length; j++) {
+          lineTokens.push([type, lines[j]])
+          if (j < lines.length - 1) {
+            flushLine(lineTokens)
+            lineTokens.length = 0
+          }
+        }
+      } else {
+        lineTokens.push(token)
+      }
+    } else {
+      lineTokens.push([type, ''])
+      flushLine(lineTokens)
+      lineTokens.length = 0
+    }
+  }
 
-function decode(str: string) {
-  return str
-    .replace('&amp;', '&')
-    .replace('&lt;', '<')
-    .replace('&gt;', '>')
-    .replace('&quot;', '"')
-    .replace('&#039;', "'")
+  if (lineTokens.length > 0) flushLine(lineTokens)
+
+  return lines.join('\n')
 }
